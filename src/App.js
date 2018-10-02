e9c1ba26fd04f531bd2084f3968d8c66c2990922
dennis cheng <c_n_cheng@yahoo.com>
2018-10-02 17:12:41 -0500

    try to make some changes

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Docker React</h1>
         </header>
         <p className="App-intro">
-          Hi There, Dennis Cheng.
+          Hi There! This is Dennis Cheng.
         </p>
       </div>
     );
